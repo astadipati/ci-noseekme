51f185cec1d939f127162a9bc6dddceabe72af8e
rama astadipati <astadipati@outlook.com>
2018-05-04 19:44:45 +0700

    Templating basic

---
+++ application/controllers/F_index.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class F_index extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('depan/f_index');
+	}
+}
